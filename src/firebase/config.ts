6194d57b12acf94082415fc94e92065994b02da4
Hemanth sai Kamatham <hemanthsaikamatham@gmail.com>
2025-10-18 03:39:09 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3295752411-28fc3",
+  "appId": "1:731545558275:web:f81180011ada783bbf1d36",
+  "apiKey": "AIzaSyDIsGo_40qjFCP14Dy1OhajjkX7wShV4MQ",
+  "authDomain": "studio-3295752411-28fc3.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "731545558275"
+};
